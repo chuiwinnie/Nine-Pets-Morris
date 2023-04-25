d11c329b490697d2a44eb29594ceb41d290cb537
suey <hlau0017@student.monash.edu>
2023-04-25 10:36:26 +1000

    Board class constructor

--- TS/Board.ts
+++ TS/Board.ts
@@ -1,1 +1,16 @@
-export class Board{}
+export class Board{
+    playingTeam: Team;
+    nonPlayingTeam: Team;
+    positions: Position[];
+
+    constructor(playingTeam: Team, nonPlayingTeam:TemplateStringsArray, positions: Position[]){
+        this.playingTeam = playingTeam;
+        this.nonPlayingTeam = nonPlayingTeam;
+        this.positions = positions;
+
+    }
+
+
+
+
+}
