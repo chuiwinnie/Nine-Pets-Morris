f963e493131e7dd81d37195ad14a3334e5a06fe4
Winnie Chui <cchu0061@student.monash.edu>
2023-04-25 15:22:32 +1000

    Implement base Display class

--- src/TS/Display.ts
+++ src/TS/Display.ts
@@ -1,3 +1,24 @@
-export class Display{
+import { Game } from "./Game";
+import { Board } from "./Board";
+import { Team } from "./Team";
 
+export class Display {
+    private displayInstance: Display;
+
+    private constructor() { }
+
+    public getInstance() {
+        if (this.displayInstance == null) {
+            this.displayInstance = new Display();
+        }
+        return this.displayInstance;
+    }
+
+    public showMainMeu() { }
+
+    public showGameList(gameList: Game[]) { }
+
+    public showBoard(gameIndex: number, board: Board) { }
+
+    public showVictory(team: Team) { }
 }
