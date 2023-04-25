ca669c47894d4aa663b88778d68f175a2f0f59d2
suey <hlau0017@student.monash.edu>
2023-04-25 13:06:58 +1000

    Game class with run, checkVictory and undo method

---
+++ TS/Game.ts
@@ -0,0 +1,78 @@
+import { Board } from "./Board"
+import { Display } from "./Display";
+
+/**
+ * Game class to implement the game and allow the game to be played
+ */
+export class Game {
+    //attriute
+    boardHistory: Board[]
+    currentBoard: Board
+
+    //Constructor
+    constructor (boardHistory: Board[], currentBoard: Board){
+        this.boardHistory = boardHistory;
+        this.currentBoard = currentBoard;
+
+    }
+
+    /**
+     * Runs the game and updates the display until a victory condition is met.
+     * @param display Display type - the display object used to update the game board
+     * @param gameIndex number - gameIndex of the current game being played
+     */
+    public run(display: Display, gameIndex: number){
+        while (this.checkVictory(this.currentBoard)){
+            let end = this.boardHistory.length-1;
+            // duplicate last board 
+            this.currentBoard = this.boardHistory[end] 
+            display.showBoard(gameIndex, this.currentBoard)
+
+            // remove own token for move
+            let removeOwnTokenAction = RemoveTokenAction(true)
+            let previousIndex = removeOwnTokenAction.perform()
+
+            // select new position for selected/removed token
+            let placeTokenAction = new PlaceTokenAction(this.currentBoard, previousIndex)
+            let newIndex = placeTokenAction.perform()
+            
+            display.showBoard(gameIndex, this.currentBoard)
+
+            // remove opponent's token if mill formed
+            if (this.currentBoard.checkMill(newIndex, true)){
+                let removeOpponentTokenAction = new RemoveTokenAction(false)
+                removeOpponentTokenAction.perform()
+            }
+
+            // switch turns
+            let switchTurnAction = new SwitchTurnAction(this.currentBoard)
+            switchTurnAction.perform()
+            
+            this.boardHistory.push(this.currentBoard)
+
+        }
+    }
+
+    /**
+     * Checks if the victory condition of the game has been met.
+     * @param Board type currentBoard - The current state of the game board.
+     * @returns boolean - True if the victory condition has not been met, false otherwise.
+     */
+    public checkVictory(currentBoard: Board){
+        if (currentBoard.nonPlayingTeam.getNumAliveTokens()<3){
+            return false
+        }
+        return true
+    }
+
+
+    /**
+     * Undoes the previous move and updates the display to reflect the previous game state.
+     * @param display - The display object used to update the game board.
+     * @param gameIndex - The index of the current game being played.
+     */
+    public undo(display: Display, gameIndex: number){
+        this.boardHistory.pop()
+        this.run(display, gameIndex)
+    }
+}
