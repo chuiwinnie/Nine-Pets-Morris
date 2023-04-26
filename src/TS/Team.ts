a398cadc8392229bee496b2229708011b55ad431
Winnie Chui <cchu0061@student.monash.edu>
2023-04-27 02:42:35 +1000

    Refactor Team class and add documentation

--- src/TS/Team.ts
+++ src/TS/Team.ts
@@ -1,58 +1,72 @@
 import { Player } from "./enums/Player.js";
 
-/**
 
-Represents a team in the game, which corresponds to a player.
-Keeps track of the number of unplaced and alive tokens for the player.
-*/
+/**
+ * This class represents a team in the game, which corresponds to a player.
+ */
 export class Team {
-    private player: Player;
-    private numUnplacedTokens: number;
-    private numaliveTokens: number;
     /**
-     * Constructor for a Team object.
-     * @param player - The player associated with this team.
+     * The player associated with this team.
+     */
+    private player: Player;
+
+    /**
+     * The number of unplaced tokens.
+     */
+    private numUnplacedTokens: number;
+
+    /**
+     * The number of alive tokens in the game.
+     */
+    private numAliveTokens: number;
+
+    /**
+     * Constructs a Team object.
+     * @param player The player associated with this team.
      */
     constructor(player: Player) {
         this.player = player;
-        this.numUnplacedTokens = 3;
-        this.numaliveTokens = 3;
-    }
-    /**
-     * Returns the number of unplaced tokens for the team.
-     * @returns The number of unplaced tokens.
-     */
-    public getNumUnplacedTokens(): number {
-        return this.numUnplacedTokens;
-    }
-    /**
-     * Returns the number of alive tokens for the team.
-     * @returns The number of alive tokens.
-     */
-    public getNumAliveTokens(): number {
-        return this.numaliveTokens;
+        this.numUnplacedTokens = 9;
+        this.numAliveTokens = 9;
     }
 
     /**
-     * Decreases the number of unplaced tokens by one.
+     * Gets the player associated with this team.
+     * @returns The player of this team.
      */
-    public placeToken(): void {
+    getPlayer(): Player {
+        return this.player;
+    }
+
+    /**
+     * Gets the number of unplaced tokens for the team.
+     * @returns The number of unplaced tokens.
+     */
+    getNumUnplacedTokens(): number {
+        return this.numUnplacedTokens;
+    }
+
+    /**
+     * Gets the number of alive tokens for the team.
+     * @returns The number of alive tokens.
+     */
+    getNumAliveTokens(): number {
+        return this.numAliveTokens;
+    }
+
+    /**
+     * Decrements the number of unplaced tokens by one.
+     */
+    placeToken(): void {
         if (this.numUnplacedTokens > 0) {
             this.numUnplacedTokens--;
         }
     }
-    /**
-     * Decreases the number of alive tokens by one.
-     */
 
-    public removeToken(): void {
-        this.numaliveTokens--;
-    }
     /**
-     * Returns the player associated with this team.
-     * @returns The player.
+     * Decrements the number of alive tokens by one.
      */
-    public getPlayer(): Player{
-        return this.player;
+    removeToken(): void {
+        this.numAliveTokens--;
     }
 }
