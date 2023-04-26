9d574d2678c855ace6eb4a2467c35836e10ac0b4
Winnie Chui <cchu0061@student.monash.edu>
2023-04-27 02:43:17 +1000

    Refactor Game class and add documentation

--- src/TS/Game.ts
+++ src/TS/Game.ts
@@ -4,73 +4,81 @@ import { PlaceTokenAction } from "./Actions/PlaceTokenAction.js"
 import { RemoveTokenAction } from "./Actions/RemoveTokenAction.js"
 import { SwitchTurnAction } from "./Actions/SwitchTurnAction.js"
 
+
 /**
- * Game class to implement the game and allow the game to be played
+ * Implement a game and allows the game to be played.
  */
 export class Game {
-    //attriute
-    boardHistory: Board[];
-    currentBoard: Board;
+    /**
+     * An array of boards representing previous turns/states of the game.
+     */
+    private boardHistory: Board[];
 
+    /**
+     * The board representing the current turn/state of the game.
+     */
+    private currentBoard: Board;
 
-    //Constructor
-    constructor(boardHistory: Board[], currentBoard: Board) {
+    /**
+     * Contructs a game with the specified board history.
+     * @param boardHistory An array of previous boards.
+     * @param currentBoard The current board of the game.
+     */
+    constructor(boardHistory: Board[], currentBoard?: Board) {
         this.boardHistory = boardHistory;
-        this.currentBoard = currentBoard;
+        this.currentBoard = currentBoard ?? boardHistory[boardHistory.length - 1];
     }
 
     /**
-     * Runs the game and updates the display until a victory condition is met.
-     * @param display Display type - the display object used to update the game board
-     * @param gameIndex number - gameIndex of the current game being played
+     * Runs the game and updates the display continuosly until a victory condition is met.
+     * @param display The Display object used to show the game.
      */
-    public run(display: Display, gameIndex: number) {
+    run(display: Display) {        
         display.showBoard(this.currentBoard);
     }
 
     /**
-
-    Executes an action on the game board, adds the resulting board to the board history,
-    and checks for victory conditions. If undoing, the previous board state is restored.
-    @param display - the Display object used to show the board and victory message
-    @param index - the index of the action to execute on the board
-    @param isUndoing - true if undoing the previous action, false otherwise
-    */
-    public action(display: Display, index: number, isUndoing: boolean) {
+     * Executes an action on the game board and adds the resulting board to the board history.
+     * @param display The Display object used to show the game.
+     * @param index The position index to execute the action on.
+     * @param isUndoing Indicates whether this is an undoing action.
+     */
+    action(display: Display, index: number, isUndoing: boolean) {
         if (isUndoing) {
-            this.undo(display)
+            this.undo(display);
         } else {
-            this.currentBoard.action(index)
-            this.boardHistory.push(this.currentBoard)
+            this.currentBoard.action(index);
+            this.boardHistory.push(this.currentBoard);
         }
-        display.showBoard(this.currentBoard)
+
+        display.showBoard(this.currentBoard);
+
         if (this.checkVictory(this.currentBoard)) {
-            display.showVictory(this.currentBoard.getNonPlayingTeam().getPlayer())
+            display.showVictory(this.currentBoard.getNonPlayingTeam().getPlayer());
         }
     }
 
     /**
-     * Checks if the victory condition of the game has been met.
-     * @param Board type currentBoard - The current state of the game board.
-     * @returns boolean - True if the victory condition has not been met, false otherwise.
+     * Checks if a victory condition of the game has been met.
+     * @param currentBoard The board representing the current game state/turn.
+     * @returns true if a victory condition has been met, false otherwise.
      */
-    public checkVictory(currentBoard: Board) {
-        let currentTeam = currentBoard.getPlayingTeam()
+    checkVictory(currentBoard: Board) {
+        let currentTeam = currentBoard.getPlayingTeam();
         if (currentTeam.getNumAliveTokens() < 3) {
-            return true
+            return true;
         }
-        return false
+        return false;
     }
 
-
     /**
-     * Undoes the previous move and updates the display to reflect the previous game state.
-     * @param display - The display object used to update the game board.
-     * @param gameIndex - The index of the current game being played.
+     * Undoes the previous move and updates the display to reflect the previous game state/turn.
+     * @param display The Display object used to show the game board.
+     * @param gameIndex The index of the currently playing game.
      */
-    public undo(display: Display) {
-        this.boardHistory.pop()
-        this.currentBoard = this.boardHistory[this.boardHistory.length - 1]
-        display.showBoard(this.currentBoard)
+    undo(display: Display) {
+        this.boardHistory.pop();
+        this.currentBoard = this.boardHistory[this.boardHistory.length - 1];
+        display.showBoard(this.currentBoard);
     }
 }
