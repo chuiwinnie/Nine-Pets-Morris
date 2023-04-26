8f2a559203644f777f9abb5800a447c340d4a0b5
Winnie Chui <cchu0061@student.monash.edu>
2023-04-27 02:41:44 +1000

    Refactore Application class and add documentation

--- src/TS/Application.ts
+++ src/TS/Application.ts
@@ -1,32 +1,29 @@
 import { Game } from "./Game.js"
 import { Display } from "./Display.js";
 import { Board } from "./Board.js";
-import { Team } from "./Team.js";
-import { Player } from "./enums/Player.js";
-import { Position } from "./Position.js";
-import { Direction } from "./enums/Direction.js";
+
 
 /**
- * This class represents an Application that manages the game state, starts new games, and handles
- * the user interface.
+ * This class represents an Application that manages and load previously played games and starts new games.
  */
 export class Application {
     /**
-     * An array of Game objects that have been created.
+     * An array of Game objects that have been previously played/created.
      */
     private gameList: Game[];
-    
+
     /**
      * The Game object that is currently being played.
      */
     private currentGame?: Game;
+
     /**
      * The Display object used to handle the user interface.
      */
     private display: Display;
-    
+
     /**
-     * The location of the text file used to load games.
+     * The location of the text file used to store and load games.
      */
     private txtFileLocation?: string;
 
@@ -34,153 +31,70 @@ export class Application {
      * The singleton instance of the Application class.
      */
     private static applicationInstance: Application;
+
     /**
-     * Constructs a new Application object with an empty game list and a new Display object.
+     * Constructs a new application with an empty game list and a Display object.
      */
     private constructor() {
         this.gameList = [];
         this.display = Display.getInstance();
     }
+
     /**
      * Returns the singleton instance of the Application class, creating it if necessary.
-     *
      * @returns The singleton instance of the Application class.
      */
-    public static getInstance() {
+    static getInstance(): Application {
         if (Application.applicationInstance == null) {
             Application.applicationInstance = new Application();
         }
         return Application.applicationInstance;
     }
+
     /**
-     * Returns the Game object that is currently being played.
-     *
+     * Gets the Game object that is currently being played.
      * @returns The current Game object, or undefined if no game is currently being played.
      */
-    public getCurrentGame() { 
+    getCurrentGame(): Game {
         return this.currentGame;
     }
 
     /**
-     * Starts a new game with an empty board and two teams (one with Player.Cat and one with Player.Dog).
+     * Starts a new game with an empty board and two teams (Cat and Dog by default).
      */
-    public startNewGame() {
+    startNewGame(): void {
         const boardHistory: Board[] = [];
-        const teams: Team[] = [new Team(Player.Cat), new Team(Player.Dog)];
-        const emptyBoard: Position[] = [];
-        for (let i = 0; i < 24; i++) {
-            emptyBoard.push(new Position(undefined, i));
-        }
-        emptyBoard[0].setNode(Direction.Right, emptyBoard[1]);
-        emptyBoard[0].setNode(Direction.Down, emptyBoard[9]);
+        boardHistory.push(new Board());
 
-        emptyBoard[1].setNode(Direction.Left, emptyBoard[0]);
-        emptyBoard[1].setNode(Direction.Right, emptyBoard[2]);
-        emptyBoard[1].setNode(Direction.Down, emptyBoard[4]);
-
-        emptyBoard[2].setNode(Direction.Left, emptyBoard[1]);
-        emptyBoard[2].setNode(Direction.Down, emptyBoard[14]);
-
-        emptyBoard[3].setNode(Direction.Right, emptyBoard[4]);
-        emptyBoard[3].setNode(Direction.Down, emptyBoard[10]);
-
-        emptyBoard[4].setNode(Direction.Left, emptyBoard[3]);
-        emptyBoard[4].setNode(Direction.Right, emptyBoard[5]);
-        emptyBoard[4].setNode(Direction.Up, emptyBoard[1]);
-        emptyBoard[4].setNode(Direction.Down, emptyBoard[7]);
-
-        emptyBoard[5].setNode(Direction.Left, emptyBoard[4]);
-        emptyBoard[5].setNode(Direction.Down, emptyBoard[13]);
-
-        emptyBoard[6].setNode(Direction.Right, emptyBoard[7]);
-        emptyBoard[6].setNode(Direction.Down, emptyBoard[11]);
-
-        emptyBoard[7].setNode(Direction.Left, emptyBoard[6]);
-        emptyBoard[7].setNode(Direction.Right, emptyBoard[8]);
-        emptyBoard[7].setNode(Direction.Up, emptyBoard[4]);
-
-        emptyBoard[8].setNode(Direction.Left, emptyBoard[7]);
-        emptyBoard[8].setNode(Direction.Down, emptyBoard[12]);
-
-        emptyBoard[9].setNode(Direction.Up, emptyBoard[0]);
-        emptyBoard[9].setNode(Direction.Right, emptyBoard[10]);
-        emptyBoard[9].setNode(Direction.Down, emptyBoard[21]);
-
-        emptyBoard[10].setNode(Direction.Left, emptyBoard[9]);
-        emptyBoard[10].setNode(Direction.Right, emptyBoard[11]);
-        emptyBoard[10].setNode(Direction.Up, emptyBoard[3]);
-        emptyBoard[10].setNode(Direction.Down, emptyBoard[18]);
-
-        emptyBoard[11].setNode(Direction.Left, emptyBoard[10]);
-        emptyBoard[11].setNode(Direction.Up, emptyBoard[6]);
-        emptyBoard[11].setNode(Direction.Down, emptyBoard[15]);
-
-        emptyBoard[12].setNode(Direction.Up, emptyBoard[8]);
-        emptyBoard[12].setNode(Direction.Right, emptyBoard[13]);
-        emptyBoard[12].setNode(Direction.Down, emptyBoard[17]);
-
-        emptyBoard[13].setNode(Direction.Left, emptyBoard[12]);
-        emptyBoard[13].setNode(Direction.Right, emptyBoard[14]);
-        emptyBoard[13].setNode(Direction.Up, emptyBoard[5]);
-        emptyBoard[13].setNode(Direction.Down, emptyBoard[20]);
-
-        emptyBoard[14].setNode(Direction.Left, emptyBoard[13]);
-        emptyBoard[14].setNode(Direction.Up, emptyBoard[2]);
-        emptyBoard[14].setNode(Direction.Down, emptyBoard[23]);
-
-        emptyBoard[15].setNode(Direction.Up, emptyBoard[11]);
-        emptyBoard[15].setNode(Direction.Right, emptyBoard[16]);
-
-        emptyBoard[16].setNode(Direction.Left, emptyBoard[15]);
-        emptyBoard[16].setNode(Direction.Right, emptyBoard[17]);
-        emptyBoard[16].setNode(Direction.Down, emptyBoard[19]);
-
-        emptyBoard[17].setNode(Direction.Left, emptyBoard[16]);
-        emptyBoard[17].setNode(Direction.Up, emptyBoard[12]);
-
-        emptyBoard[18].setNode(Direction.Up, emptyBoard[10]);
-        emptyBoard[18].setNode(Direction.Right, emptyBoard[19]);
-
-        emptyBoard[19].setNode(Direction.Left, emptyBoard[18]);
-        emptyBoard[19].setNode(Direction.Right, emptyBoard[20]);
-        emptyBoard[19].setNode(Direction.Up, emptyBoard[16]);
-        emptyBoard[19].setNode(Direction.Down, emptyBoard[22]);
-
-        emptyBoard[20].setNode(Direction.Left, emptyBoard[19]);
-        emptyBoard[20].setNode(Direction.Up, emptyBoard[13]);
-
-        emptyBoard[21].setNode(Direction.Up, emptyBoard[9]);
-        emptyBoard[21].setNode(Direction.Right, emptyBoard[22]);
-
-        emptyBoard[22].setNode(Direction.Left, emptyBoard[21]);
-        emptyBoard[22].setNode(Direction.Right, emptyBoard[23]);
-        emptyBoard[22].setNode(Direction.Up, emptyBoard[19]);
-
-        emptyBoard[23].setNode(Direction.Left, emptyBoard[22]);
-        emptyBoard[23].setNode(Direction.Up, emptyBoard[14]);
-
-        boardHistory.push(new Board(teams, 0, emptyBoard, 1));
-
-        const newGame = new Game(boardHistory, boardHistory[boardHistory.length-1]);
+        const newGame = new Game(boardHistory);
         this.gameList.push(newGame);
 
         const newGameIndex = this.gameList.length - 1;
         this.loadGame(newGameIndex);
     }
-    /**
 
-    Loads the game specified by gameIndex from the gameList and runs it with the given display.
-    @param gameIndex - The index of the game to be loaded from the gameList.
-    */
-    private loadGame(gameIndex: number) { 
+    /**
+     * Loads the game specified by gameIndex from the gameList and runs it on display.
+     * @param gameIndex The index of the game to be loaded from the gameList.
+     */
+    loadGame(gameIndex: number): void {
         this.currentGame = this.gameList[gameIndex];
-        this.currentGame.run(this.display, gameIndex);
+        this.currentGame.run(this.display);
     }
 
-    private loadFromFile() { }
+    /**
+     * Loads games from a TXT file.
+     */
+    loadFromFile(): void { }
 
-    public exit(gameIndex: number) { }
+    /**
+     * Exits the current game.
+     * @param gameIndex The index of the current game
+     */
+    exit(gameIndex: number): void { }
 }
 
+
+// This application currently can only start new games (Sprint 1).
 const application = Application.getInstance();
 application.startNewGame();
