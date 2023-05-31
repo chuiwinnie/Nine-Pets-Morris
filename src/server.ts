0d7c6e0738c93bd590234cb2037144f68bcc817c
suey <hlau0017@student.monash.edu>
2023-05-31 11:45:19 +1000

    update server to read/load data

--- src/server.ts
+++ src/server.ts
@@ -14,16 +14,23 @@ const __dirname = dirname(__filename);
 // Serve static files from the dist directory
 app.use('/out', express.static(path.join(__dirname, '..', 'out')));
 app.use('/assets',express.static(path.join(__dirname,'..', 'assets')));
-//app.use(express.static(path.join(__dirname, 'Application.css')));
+
 app.use(express.static(path.join(__dirname,'..', 'Application.css')));
 app.get('/Application.css', (req, res) => {
   res.setHeader('Content-Type', 'text/css');
   res.sendFile(path.join(__dirname, '../Application.css'));
 });
 
+
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../menu.html'));
+});
+app.get('/application.html', (req, res) => {
   res.sendFile(path.join(__dirname, '../Application.html'));
 });
+
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
@@ -36,22 +43,49 @@ app.post('/save', (req, res) => {
   console.log(req.body)
   if (req.body) {
     const boardStrings = req.body.split('\n');
-    const filePath = path.join(__dirname, 'data.txt');
+    const filePath = path.join(__dirname, '..','data.txt');
 
-    // Join all board strings with newline characters
-    const fileContent = boardStrings.join('\n');
-
-    fs.writeFile(filePath, fileContent, { flag: 'w' }, (err) => {
-      if (err) {
-        console.error('Error writing file:', err);
-        res.status(500).send('Error saving data');
-      } else {
+    boardStrings.forEach((boardString) => {
+      fs.writeFileSync(path.join(__dirname, '..','data.txt'), boardString + '\n', { flag: 'a' });
+      });
         console.log('Text file written successfully');
         res.send('Data saved successfully');
-      }
-    })
   }else {
     console.error('Received empty JSON data');
     res.status(400).send('Received empty JSON data');
   }
+});
+
+
+app.get('/load', (req, res) => {
+  fs.readFile(path.join(__dirname,'..','data.txt'), 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading data file:', err);
+      res.status(500).send('Error loading game data');
+    } else {
+      const gameEntries = data.trim().split('\n');
+      const gameList = [];
+      let currentGame = { name: '', data: '' };
+      for (const gameEntry of gameEntries) {
+          if (gameEntry[0] !== '{') {
+            // New game entry detected
+            if (currentGame.name !== '' && currentGame.data !== '') {
+              gameList.push(currentGame);
+            }
+            currentGame = { name: gameEntry.trim(), data: '' };
+          } else {
+            // Continue existing game entry
+            currentGame.data = gameEntry;
+          }
+        }
+
+        // Add the last game entry
+        if (currentGame.name !== '' && currentGame.data !== '') {
+          gameList.push(currentGame);
+        }
+
+        //res.json(gameList);     
+        res.send(gameList);
+      }
+  });
 });
